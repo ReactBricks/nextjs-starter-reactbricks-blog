ce689e336b424d7e8adb791f9ed70f39c768d9b9
Cecilia Panicali <cecilia@f2.net>
2021-12-17 16:34:48 +0100

    BlogList - edited page styles.

--- components/BlogListItem.tsx
+++ components/BlogListItem.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Link from 'next/link'
+import { FiArrowRight } from 'react-icons/fi'
 
 interface BlogListItemProps {
   title: string
@@ -8,13 +10,18 @@ interface BlogListItemProps {
 
 const BlogListItem: React.FC<BlogListItemProps> = ({ title, href, content }) => {
   return (
-    <div className="flex-1 group py-4">
-      <a href={href} className="block relative text-gray-900 h-full w-full">
-        <h3 className="font-bold text-2xl capitalize group-hover:text-pink-600">{title}</h3>
+    <Link href={href}>
+      <a className="flex-1 block group py-4 text-gray-900 hover:bg-gray-50 transition-colors duration-300 p-6 -m-6 rounded">
+        <h3 className="font-bold text-2xl">
+          {title}
+        </h3>
         <p className="mt-4 leading-6 text-lg">{content}</p>
-        <div className="mt-6 font-bold">Read More {`->`} </div>
+        <div className="flex items-center mt-4 text-cyan-500 group-hover:text-cyan-600 font-semibold">
+          <div className="mr-2 group-hover:mr-4 transition-all duration-300">Read More</div>
+          <FiArrowRight />
+        </div>
       </a>
-    </div>
+    </Link>
   )
 }
 
--- pages/blog-list.tsx
+++ pages/blog-list.tsx
@@ -1,21 +1,14 @@
 import { GetStaticProps } from 'next'
 import Head from 'next/head'
 import { useContext, useState } from 'react'
-import {
-  cleanPage,
-  fetchPage,
-  fetchPages,
-  PageViewer,
-  ReactBricksContext,
-  types,
-} from 'react-bricks/frontend'
-import { FaArrowRight } from 'react-icons/fa'
+import { cleanPage, fetchPage, fetchPages, ReactBricksContext, types } from 'react-bricks/frontend'
 import ErrorNoHomePage from '../components/errorNoHomePage'
 import ErrorNoKeys from '../components/errorNoKeys'
 
 import BlogListItem from '../components/BlogListItem'
 import Layout from '../components/layout'
 import config from '../react-bricks/config'
+import Link from 'next/link'
 
 interface HomeProps {
   page: types.Page
@@ -55,9 +48,11 @@ const BlogList: React.FC<HomeProps> = ({ posts, page, error }) => {
           Blog
         </span>
       </h1>
-      <div className="max-w-6xl mx-auto px-8 pt-16 flex space-x-24">
+      <div className="max-w-6xl mx-auto px-8 py-16 flex space-x-24">
         <section className="flex-2 space-y-8">
-          <h2 className="text-pink-500 uppercase mb-8 tracking-widest font-bold">Heading</h2>
+          <h2 className="text-pink-500 uppercase mb-8 tracking-widest font-bold">
+            Recently published
+          </h2>
           {posts
             ?.filter((post) =>
               post.tags.find((tag) => (typeof filter === 'boolean' ? filter : tag === filter))
@@ -79,24 +74,25 @@ const BlogList: React.FC<HomeProps> = ({ posts, page, error }) => {
               {tags
                 ?.filter((tag) => tag !== 'popular')
                 .map((tag) => (
-                  <div
-                    key={tag}
-                    className="text-sm font-bold relative isolate rounded-md px-2 py-1 group cursor-pointer flex justify-center mr-2 mb-2"
-                    onClick={() => setFilter(tag)}
-                  >
-                    <div
-                      className="transform duration-200 group-hover:scale-105 absolute inset-0 bg-cyan-100 group-hover:bg-cyan-200 rounded-md px-2 py-1 border border-solid border-cyan-200"
-                      style={{ zIndex: -1 }}
-                    />
-                    {tag}
-                  </div>
+                  <Link href="/" key={tag}>
+                    <a
+                      className="inline-block text-sm font-bold mr-2 mb-2 transform duration-200 text-cyan-800 bg-cyan-100 hover:bg-cyan-200 hover:text-cyan-900 rounded-md px-2 py-1"
+                      onClick={() => setFilter(tag)}
+                    >
+                      <div
+                        className=""
+                        style={{ zIndex: -1 }}
+                      />
+                      {tag}
+                    </a>
+                  </Link>
                 ))}
               <div
                 className="text-sm font-bold relative isolate rounded-md px-2 py-1 group cursor-pointer flex justify-center mb-2"
                 onClick={() => setFilter(true)}
               >
                 <div
-                  className="transform duration-200 group-hover:scale-105 absolute inset-0 bg-purple-100 group-hover:bg-purple-200 rounded-md px-2 py-1 border border-solid border-purple-200"
+                  className="transform duration-200 group-hover:scale-105 absolute inset-0 text-purple-900 bg-purple-100 group-hover:bg-purple-200 rounded-md px-2 py-1"
                   style={{ zIndex: -1 }}
                 />
                 Reset tags
@@ -110,17 +106,12 @@ const BlogList: React.FC<HomeProps> = ({ posts, page, error }) => {
               {posts
                 ?.filter((post) => post.tags.find((tag) => tag === 'popular'))
                 .map((post) => (
-                  <li key={post.id} className="">
+                  <li key={post.id}>
                     <a
                       href={post.slug}
-                      className="flex space-x-2 items-center group cursor-pointer"
+                      className="text-gray-900 hover:text-cyan-600 font-bold text-lg leading-10 transition-colors"
                     >
-                      <span className="transform duration-300 -translate-x-1 group-hover:translate-x-1">
-                        <FaArrowRight className="text-pink-600 group-hover:text-pink-700" />
-                      </span>
-                      <div className="flex-1 text-gray-900 font-bold text-lg leading-10 capitalize">
-                        {post.name}
-                      </div>
+                      {post.name}
                     </a>
                   </li>
                 ))}
