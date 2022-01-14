f2ceb839e63df357559c7e9efb4b926049ba334b
Giorgio Tognoli <giorgio@f2.net>
2022-01-14 15:47:47 +0100

    fixed tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,3 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
-const colors = require('tailwindcss/colors')
-
 module.exports = {
   content: [
     './pages/**/*.tsx',
@@ -9,12 +6,6 @@ module.exports = {
     './node_modules/react-bricks-ui/**/*.js',
   ],
   darkMode: 'class',
-  theme: {
-    flex: {
-      1: '1 1 0%',
-      2: '2 2 0%',
-    },
-    extend: {},
-  },
-  variants: {},
+  theme: {},
 }
+
