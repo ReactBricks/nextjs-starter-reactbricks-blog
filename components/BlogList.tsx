34ed0a8bfc8fe19fae7c46bb7dce7636ab6df2c5
Giorgio Tognoli <giorgio@f2.net>
2022-05-27 17:21:40 +0200

    Updated react-bricks-ui and interfaces layout and bloglist

--- components/BlogList.tsx
+++ components/BlogList.tsx
@@ -1,4 +1,10 @@
-const BlogList = ({ children }) => {
+import { ReactNode } from 'react'
+
+interface BlogListProps {
+  children?: ReactNode
+}
+
+const BlogList: React.FC<BlogListProps> = ({ children }) => {
   return <div className="max-w-6xl mx-auto px-8 pt-16 flex">{children}</div>
 }
 
